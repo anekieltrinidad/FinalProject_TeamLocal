f401d6e96b367d138ec6d52d46cb6847c2e8c021
anekieltrinidad <tanekiel@gmail.com>
2021-03-12 21:42:52 +0800

    Added initial files. Contains login and register pages.

---
+++ TeamLocal/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TeamLocal.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+
+namespace TeamLocal.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
